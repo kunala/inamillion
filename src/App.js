1f65060a9c5a6aab6bf1ebfd72e59f9e9aff2fd5
kunala <kunala@berkeley.edu>
2021-01-21 23:35:56 -0800

    finishing touches on frontpage

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import { Icon } from '@iconify/react';
 import spotifyIcon from '@iconify-icons/mdi/spotify';
 import appleMusic from '@iconify-icons/cib/apple-music';
 import Link from '@material-ui/core/Link';
+import ReactTypingEffect from 'react-typing-effect';
+
 
 function App() {
   return (
@@ -16,39 +18,43 @@ function App() {
 
     <div className="milli">
       <Container maxWidth="xs">
-        <Box my={19}>
+        <Box my={10}>
 
           <center>
             <img src={markjoy} width="100%"/>
             
-
-
-          {/*<h4 className="white">
-            ONE IN A MILLION
-          </h4>*/}
           <Box lineHeight={2} fontWeight="900" fontSize="h6.fontSize" className="white" m={1}>
           ONE IN A MILLION
           </Box>
           <Box lineHeight={1} fontWeight="700" fontSize="body1.fontSize" className="white" m={1}>
-          12 February
+          Releasing 12 February
           </Box>
           <br />
           {/*<Button variant="contained" color="primary" disableElevation>
           Disable elevation
           </Button>*/}
             <Box m={1}>
-              <Button variant="outlined" className="spot" fullWidth><Icon icon={spotifyIcon} height="25" />Presave</Button>
+              <Button variant="outlined" className="spot" fullWidth><Icon icon={spotifyIcon} height="25" />&nbsp;&nbsp;Presave</Button>
             </Box>
 
             <Box m={1}>
-              <Button variant="outlined" className="apple" fullWidth><Icon icon={appleMusic} height="25" />Preadd</Button>
-            }
+              <Button variant="outlined" className="apple" fullWidth><Icon icon={appleMusic} height="20" />&nbsp;&nbsp;&nbsp;Preadd</Button>
+            
             </Box>
             
-          <br /><br /><br /><br /><br /><br />
+          <br />
+          <Box lineHeight={1} fontWeight="600" fontSize="h3.fontSize" className="white" m={1}><ReactTypingEffect
+        text={["384"]} eraseDelay={20000} cursor= "|" typingDelay = {250}
+      /></Box>
+          <Box lineHeight={.1} fontWeight="400" fontSize="Button.fontSize" className="white" m={1}>
+          JOINED THE MARKJOY ARMY
+          </Box>
 
-          <Box lineHeight={2} fontWeight="400" fontSize="overline.fontSize" className="white" m={1}>
-          MADE WITH ♥ BY @<Link href="https://instagram.com/kunala" color="inherit"><u>KUNALA</u></Link>
+
+          <br />
+
+          <Box lineHeight={2} fontWeight="300" fontSize="overline.fontSize" className="white" m={1}>
+          MADE WITH <span className="red">♥</span> BY @<Link href="https://instagram.com/kunala" color="inherit"><u>KUNALA</u></Link>
           </Box>
 
           </center>
