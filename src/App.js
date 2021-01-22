228dab36febcadbfa1f3c8bf31a6d3c75fd898ba
kunala <kunala@berkeley.edu>
2021-01-21 16:38:41 -0800

    Started l&f change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
+import markjoy from './markjoy.jpeg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+    <div className="milli">
+      
+        <center>
+        <h4>
           Version 2 by kunala.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        </h4>
+        </center>
+      
     </div>
   );
 }
